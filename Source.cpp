b44aea4d30a7a42f2fe5884d15f4f6ec2fadcdf7
dexterlab_5 <103762164+DexterCodes5@users.noreply.github.com>
2022-09-11 10:53:01 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+
+using namespace std::literals;
+
+std::shared_mutex mx;
+
+static double d{ 79.8 };
+
+void write(double d2) {
+	std::lock_guard<std::shared_mutex> lg(mx);
+	d /= (7 + d2);
+	std::this_thread::sleep_for(1s);
+}
+
+void read() {
+	std::shared_lock<std::shared_mutex> sl(mx);
+	std::cout << d << "\n";
+}
+
+int main() {
+	std::vector<std::thread> v;
+
+	// Releasing a couple
+	for (int i = 0; i < 5; ++i)
+		v.emplace_back(read);
+
+	for (int i = 0; i < 2; ++i)
+		v.emplace_back(write, i * 0.15);
+
+	for (int i = 0; i < 5; ++i)
+		v.emplace_back(read );
+
+	for (int i = 0; i < 2; ++i)
+		v.emplace_back(write, i * 0.15);
+
+	for (int i = 0; i < 5; ++i)
+		v.emplace_back(read);
+
+	std::for_each(v.begin(), v.end(), [](auto& t) {t.join(); });
+}
